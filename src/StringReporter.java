18dd63dad006c26ece232a26b8788d93b66f6952
gsteinLTU <gstein@ltu.edu>
2015-09-03 17:30:18 -0400

    Initial commit

---
+++ src/StringReporter.java
@@ -0,0 +1,49 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class StringReporter {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	/**
+	 * Determines unique words and outputs them, their lengths, and their frequencies
+	 * @param file The File to load from
+	 */
+	static void handleFile(File file){
+		
+	}
+	
+	/**
+	 * Determine unique words in the document
+	 * @param document String of document text
+	 * @return List of unique words, in alphabetical order
+	 */
+	static List<String> getUniqueWords(String document){
+		return new ArrayList<String>();
+	}
+	
+	/**
+	 * Count occurrences of a word in the document
+	 * @param document String of document text
+	 * @param word Word to count occurrences of
+	 * @return Number of times word appears in the document
+	 */
+	static int getWordFrequency(String document, String word){
+		return 0;
+	}
+	
+	/**
+	 * Determine the length of a word
+	 * @param word Word to count letters in
+	 * @return Number of letters in word
+	 */
+	static int getWordLength(String word){
+		return 0;
+	}	
+
+}
