16d3aff1b89bcb3a30a0bf40c11af94b453c1ef5
gsteinLTU <gstein@ltu.edu>
2015-09-14 16:14:16 -0400

    Completed: +Prompt user for file +Load contents of user file +Display results to user

--- src/edu/ltu/stringreporter/StringReporter.java
+++ src/edu/ltu/stringreporter/StringReporter.java
@@ -1,22 +1,83 @@
 package edu.ltu.stringreporter;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.swing.JFileChooser;
 
 
 public class StringReporter {
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Welcome to StringReporter!");
+		System.out.println("Please select a .txt file to create a report from...");
+		
+		// Wait one second to allow user to read output
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		// Prompt for input file
+		JFileChooser openDialog = new JFileChooser();
+		
+		if(openDialog.showOpenDialog(null) == JFileChooser.APPROVE_OPTION){
+			// User has selected a file
+			try{
+				handleFile(openDialog.getSelectedFile());
+			} catch(IllegalArgumentException e){
+				System.out.println("Invalid file type selected. This program requires a .txt file. The program will now exit.");
+			} catch(IOException e){
+				System.out.println("Error reading file. The program will now exit.");
+			}
+			
+		} else {
+			// User has not selected a file
+			System.out.println("No file selected. The program will now exit.");	
+		}
 	}
 	
 	/**
 	 * Determines unique words and outputs them, their lengths, and their frequencies
 	 * @param file The File to load from
+	 * @throws IllegalArgumentException If file is not supported
+	 * @throws IOException If error occurs reading from file
 	 */
-	public static void handleFile(File file){
+	public static void handleFile(File file) throws IllegalArgumentException, IOException {
+		if(file.isDirectory() || !file.getName().endsWith(".txt")){
+			throw new IllegalArgumentException("File must be a .txt file");
+		}
+		
+		// Get file content
+		BufferedReader br = new BufferedReader(new FileReader(file));
+		StringBuilder builder = new StringBuilder();
+		String line = br.readLine();
+		
+		// Read file line by line
+		while(line != null){	
+			builder.append(line);
+			builder.append(" ");		
+			line = br.readLine();
+		}
+		
+		String fileContents = builder.toString();
+		
+		// Display output
+		ArrayList<String> words = getUniqueWords(fileContents);
+		
+		System.out.println("Words in document:");
+		
+		for(String word : words){
+			System.out.print(word);
+			System.out.print(" (" + getWordLength(word) + " characters long): ");
+			System.out.println(getWordFrequency(fileContents, word) + " occurances");
+		}
 		
 	}
 	
